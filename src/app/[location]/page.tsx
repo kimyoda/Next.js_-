8522cf34a0477ae5d8d4c0e6be4b8b29c8e790da
kimyoda <rladygks1210@naver.com>
2024-12-21 03:46:06 +0900

    [feat] page move, link, useRouter

--- src/app/[location]/page.tsx
+++ src/app/[location]/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import HomeButton from "../components/HomeButton";
+
 type Props = {
   params: {
     location: string;
@@ -5,11 +9,12 @@ type Props = {
 };
 
 export default function Detail({ params }: Props) {
-  const name = params.location === "seoul" ? "서울" : "부산";
+  const name = params.location === "seoul" ? "서울" : "";
 
   return (
     <>
       <h1>{name}의 3일 예보</h1>
+      <HomeButton />
     </>
   );
 }
