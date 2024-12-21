c813cfc9222b0d4c7a60b252dfe4d7bf645f6bdc
kimyoda <rladygks1210@naver.com>
2024-12-21 23:26:21 +0900

    [feat] css module add, global css add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 
+import style from "./style.module.css";
+
 export default function Home() {
   return (
     <>
       <h1>main</h1>
-      <ul>
+      <ul className={style.list}>
         <li>
           <Link href="/seoul">서울</Link>
         </li>
@@ -12,7 +14,7 @@ export default function Home() {
           <Link href="/newyork">뉴욕</Link>
         </li>
         <li>
-          <Link href="london">런던</Link>
+          <Link href="/london">런던</Link>
         </li>
       </ul>
     </>
